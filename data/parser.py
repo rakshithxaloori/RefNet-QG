6b754b52dff5c9b25ae743ec8b1a202160ca8600
Rakshith Aloori <rakshithaloori10sa@gmail.com>
2020-05-26 10:39:36 +0530

    Remove [number] substrings

--- data/parser.py
+++ data/parser.py
@@ -5,11 +5,12 @@ import os
 import sys
 from copy import deepcopy
 
-from remove_non_language import remove_non_language
+from remove import remove_non_language, remove_numbers
 
 # Generalize for dev, train from sys.argv
 if len(sys.argv) != 5:
-    sys.exit("Usage: python parser.py data_json_file data\'s_type language language_two_letter_code")
+    sys.exit(
+        "Usage: python parser.py data_json_file data\'s_type language language_two_letter_code")
 
 jsonFileName = sys.argv[1]
 dataType = sys.argv[2]
@@ -32,7 +33,8 @@ try:
 except OSError as error:
     print(error)
 
-languageParsedDirectoryPath = os.getcwd() + os.sep + "parsedData" + os.sep + language + "_" + dataType
+languageParsedDirectoryPath = os.getcwd() + os.sep + "parsedData" + \
+    os.sep + language + "_" + dataType
 try:
     os.mkdir(languageParsedDirectoryPath)
 except OSError as error:
@@ -40,7 +42,8 @@ except OSError as error:
 
 # Deleting the files if they already exist
 if os.path.exists(os.path.join(languageParsedDirectoryPath, dataType + "_" + language + "_data.json")):
-    os.remove(os.path.join(languageParsedDirectoryPath, dataType + "_" + language + "_data.json"))
+    os.remove(os.path.join(languageParsedDirectoryPath,
+                           dataType + "_" + language + "_data.json"))
 
 if os.path.exists(os.path.join(languageParsedDirectoryPath, dataType + "_context")):
     os.remove(os.path.join(languageParsedDirectoryPath, dataType + "_context"))
@@ -62,32 +65,36 @@ for eachData in data["data"]:
             # answers --> list of dicts
             # id
         # context
-    
+
     # Creating a list of data with the specified language
     copiedEachData = deepcopy(eachData)
     # Removing non-language words
-    data_list.append(remove_non_language(copiedEachData["paragraphs"][0]["context"], language_code))
+    data_list.append(remove_non_language(
+        copiedEachData["paragraphs"][0]["context"], language_code))
 
-    contextFile = open(os.path.join(languageParsedDirectoryPath, dataType + "_context"), 'a+')
-    questionFile = open(os.path.join(languageParsedDirectoryPath, dataType + "_question"), 'a+')
-    answerFile = open(os.path.join(languageParsedDirectoryPath, dataType + "_answer"), 'a+')
+    contextFile = open(os.path.join(
+        languageParsedDirectoryPath, dataType + "_context"), 'a+')
+    questionFile = open(os.path.join(
+        languageParsedDirectoryPath, dataType + "_question"), 'a+')
+    answerFile = open(os.path.join(
+        languageParsedDirectoryPath, dataType + "_answer"), 'a+')
 
-    
     for paragraph in eachData["paragraphs"]:
         for qa in paragraph["qas"]:
             # Append to the three files
             # Context file -- paragraph[context]
-            contextFile.write(remove_non_language(paragraph["context"], language_code))
+            sentence = paragraph["context"]
+            sentence = remove_numbers(sentence)
+            contextFile.write(remove_non_language(sentence, language_code))
             contextFile.write('\n')
-            
+
             # Questions file -- qa["question"]
             questionFile.write(qa["question"])
             questionFile.write('\n')
-            
+
             # Answers file -- qa["answers"][0]["text"] -- choosing the first answer
             answerFile.write(qa["answers"][0]["text"])
             answerFile.write('\n')
-            
 
     # Closing the files
     contextFile.close()
--- data/remove.py
+++ data/remove.py
@@ -4,13 +4,14 @@ from nltk.tokenize import wordpunct_tokenize, word_tokenize
 from langdetect import detect
 from langdetect.lang_detect_exception import LangDetectException
 
+
 def remove_non_language(sentence, language_code):
     """ Removes the non-language words from a sentence. """
     for word in word_tokenize(sentence):
         # Ignoring punctuations
         if word in string.punctuation or word in string.digits:
             continue
-        
+
         # Punctuations throw an exception
         try:
             if detect(word) != language_code:
@@ -18,4 +19,18 @@ def remove_non_language(sentence, language_code):
         except LangDetectException:
             # Probably, a punctuation is detected
             pass
+    return sentence
+
+
+def remove_numbers(sentence):
+    """ Removes the [number] substring from sentence. """
+    while True:
+        index = sentence.find("[")
+        if index == -1:
+            break
+        # Remove a set of [number]
+        sentence.replace(sentence[index+2], '')
+        sentence.replace(sentence[index+1], '')
+        sentence.replace(sentence[index], '')
+
     return sentence
