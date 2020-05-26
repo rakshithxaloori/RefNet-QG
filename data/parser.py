65017b012b9119139447c54f742c1dbab570ddf2
Rakshith Aloori <rakshithaloori10sa@gmail.com>
2020-05-26 13:21:57 +0530

    Add code to find the sentence of answer

--- data/parser.py
+++ data/parser.py
@@ -7,6 +7,8 @@ from copy import deepcopy
 
 from remove import remove_non_language, remove_numbers
 
+from nltk.tokenize import sent_tokenize
+
 # Generalize for dev, train from sys.argv
 if len(sys.argv) != 5:
     sys.exit(
@@ -45,14 +47,17 @@ if os.path.exists(os.path.join(languageParsedDirectoryPath, dataType + "_" + lan
     os.remove(os.path.join(languageParsedDirectoryPath,
                            dataType + "_" + language + "_data.json"))
 
-if os.path.exists(os.path.join(languageParsedDirectoryPath, dataType + "_context")):
-    os.remove(os.path.join(languageParsedDirectoryPath, dataType + "_context"))
+if os.path.exists(os.path.join(languageParsedDirectoryPath, dataType + "_context.txt")):
+    os.remove(os.path.join(languageParsedDirectoryPath, dataType + "_context.txt"))
 
-if os.path.exists(os.path.join(languageParsedDirectoryPath, dataType + "_question")):
-    os.remove(os.path.join(languageParsedDirectoryPath, dataType + "_question"))
+if os.path.exists(os.path.join(languageParsedDirectoryPath, dataType + "_question.txt")):
+    os.remove(os.path.join(languageParsedDirectoryPath, dataType + "_question.txt"))
 
-if os.path.exists(os.path.join(languageParsedDirectoryPath, dataType + "_answer")):
-    os.remove(os.path.join(languageParsedDirectoryPath, dataType + "_answer"))
+if os.path.exists(os.path.join(languageParsedDirectoryPath, dataType + "_answer.txt")):
+    os.remove(os.path.join(languageParsedDirectoryPath, dataType + "_answer.txt"))
+
+if os.path.exists(os.path.join(languageParsedDirectoryPath, dataType + "_answer_sentence.txt")):
+    os.remove(os.path.join(languageParsedDirectoryPath, dataType + "_answer_sentence.txt"))
 
 count = 0
 for eachData in data["data"]:
@@ -74,10 +79,10 @@ for eachData in data["data"]:
     # Creating a list of data with the specified language
     copiedEachData = deepcopy(eachData)
     # Removing non-language words
-    sentence = copiedEachData["paragraphs"][0]["context"]
-    sentence = remove_numbers(sentence)
-    sentence = remove_non_language(sentence, language_code)
-    data_list.append(remove_non_language(sentence, language_code))
+    context = copiedEachData["paragraphs"][0]["context"]
+    context = remove_numbers(context)
+    context = remove_non_language(context, language_code)
+    data_list.append(remove_non_language(context, language_code))
 
     contextFile = open(os.path.join(
         languageParsedDirectoryPath, dataType + "_context.txt"), 'a+')
@@ -85,21 +90,41 @@ for eachData in data["data"]:
         languageParsedDirectoryPath, dataType + "_question.txt"), 'a+')
     answerFile = open(os.path.join(
         languageParsedDirectoryPath, dataType + "_answer.txt"), 'a+')
+    
+    answerSentenceFile = open(os.path.join(
+        languageParsedDirectoryPath, dataType + "_answer_sentence.txt"), 'a+')
 
     for paragraph in eachData["paragraphs"]:
         for qa in paragraph["qas"]:
             # Append to the three files
             # Context file -- paragraph[context]
-            contextFile.write(sentence)
+            contextFile.write(context)
             contextFile.write('\n')
 
             # Questions file -- qa["question"]
-            questionFile.write(qa["question"])
+            question = qa["question"]
+            questionFile.write(question)
             questionFile.write('\n')
 
             # Answers file -- qa["answers"][0]["text"] -- choosing the first answer
-            answerFile.write(qa["answers"][0]["text"])
+            answer = qa["answers"][0]["text"]
+            answerFile.write(answer)
             answerFile.write('\n')
+
+            # Find the answer in the context
+            foundFlag = False
+            for sentence in sent_tokenize(context):
+                if sentence.find(answer) != -1:
+                    # Answer found
+                    foundFlag = True
+                    answerSentenceFile.write(sentence)
+                    answerSentenceFile.wirte('\n')
+                    break
+            
+            if not foundFlag:
+                # The answer is not found
+                answerSentenceFile.write("NOT FOUND")
+                answerSentenceFile.write('\n')
 
     # Closing the files
     contextFile.close()
